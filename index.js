15dc873105cdc01673d9dcccd2671bb986cd9232
Jason Rowland <jrowland@us.ci.org>
2021-06-25 12:31:03 -0500

    Formatting

--- index.js
+++ index.js
@@ -2,8 +2,8 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
-import App from './src/App';
-import {name as appName} from './package.json';
+import { AppRegistry } from 'react-native'
+import App from './src/App'
+import { name as appName } from './package.json'
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => App)
